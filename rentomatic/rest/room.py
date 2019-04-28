e775c220df35b14e2c0a16a9998545918f00bb58
Alexander G. Fowler <alexfowler@gmail.com>
2019-04-28 23:49:34 +0800

    initial commit. Completed until Part 2 - Chapter 3

---
+++ rentomatic/rest/room.py
@@ -0,0 +1,65 @@
+import json
+
+from flask import Blueprint, Response, request
+
+from rentomatic.repository import memrepo as mr
+from rentomatic.use_cases import room_list_use_case as uc
+from rentomatic.serializers import room_json_serializer as ser
+from rentomatic.request_objects import room_list_request_object as req
+from rentomatic.response_objects import response_objects as res
+
+blueprint = Blueprint('room', __name__)
+
+STATUS_CODES = {
+    res.ResponseSuccess.SUCCESS: 200,
+    res.ResponseFailure.RESOURCE_ERROR: 404,
+    res.ResponseFailure.PARAMETERS_ERROR: 400,
+    res.ResponseFailure.SYSTEM_ERROR: 500
+}
+
+room1 = {
+    'code': 'f853578c-fc0f-4e65-81b8-566c5dffa35a',
+    'size': 215,
+    'price': 39,
+    'longitude': -0.09998975,
+    'latitude': 51.75436293,
+}
+
+room2 = {
+    'code': 'fe2c3195-aeff-487a-a08f-e0bdc0ec6e9a',
+    'size': 405,
+    'price': 66,
+    'longitude': 0.18228006,
+    'latitude': 51.74640997,
+}
+
+room3 = {
+    'code': '913694c6-435a-4366-ba0d-da5334a611b2',
+    'size': 56,
+    'price': 60,
+    'longitude': 0.27891577,
+    'latitude': 51.45994069,
+}
+
+
+@blueprint.route('/rooms', methods=['GET'])
+def room():
+
+    qrystr_params = {
+        'filters': {},
+    }
+
+    for arg, values in request.args.items():
+        if arg.startswith('filter_'):
+            qrystr_params['filters'][arg.replace('filter_', '')] = values
+
+    request_object = req.RoomListRequestObject.from_dict(qrystr_params)
+
+    repo = mr.MemRepo([room1, room2, room3])
+    use_case = uc.RoomListUseCase(repo)
+
+    response = use_case.execute(request_object)
+
+    return Response(json.dumps(response.value, cls=ser.RoomJsonEncoder),
+                    mimetype='application/json',
+                    status=STATUS_CODES[response.type])
